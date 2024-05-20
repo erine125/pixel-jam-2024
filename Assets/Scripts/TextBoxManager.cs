82b6b8a500c42bc37686e66b18ae2054124a7d42
erine125 <catherine.wang.qq@gmail.com>
2024-05-20 06:40:49 -0700

    added textboxes

---
+++ Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TextboxManager : MonoBehaviour
+{
+    public GameObject[] textboxes; // Assign all your textboxes here in order
+    public AudioSource audiosource;
+    private int currentTextboxIndex = 0;
+
+    void Start()
+    {
+        HideAllTextboxes();
+        ShowNextTextbox();
+    }
+
+    void HideAllTextboxes()
+    {
+        foreach (GameObject textbox in textboxes)
+        {
+            textbox.SetActive(false);
+        }
+    }
+
+    public void ShowNextTextbox()
+    {
+        if (currentTextboxIndex < textboxes.Length)
+        {
+            textboxes[currentTextboxIndex].SetActive(true);
+            Button btn = textboxes[currentTextboxIndex].GetComponent<Button>();
+            btn.onClick.AddListener(OnTextboxClicked);
+        }
+        else
+        {
+            // All textboxes have been shown, allow access to the level
+            gameObject.SetActive(false); // Optionally disable the manager or the UI container
+        }
+    }
+
+    void OnTextboxClicked()
+    {
+        // Hide the current textbox
+        audiosource.Play();
+        textboxes[currentTextboxIndex].SetActive(false);
+        
+
+        // Increment the index to point to the next textbox
+        currentTextboxIndex++;
+
+        // Show the next textbox
+        ShowNextTextbox();
+    }
+}
